4871881115159d0b74f243f79a2eb1d260953b19
francisadhi <adnugrohoo@gmail.com>
2018-04-27 20:57:28 +0700

    add db pulsa

---
+++ app/db/db_pulsa.sql
@@ -0,0 +1,35 @@
+/*
+SQLyog Community v8.3 
+MySQL - 5.5.5-10.1.30-MariaDB : Database - pulsa
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `user_admin` */
+
+DROP TABLE IF EXISTS `user_admin`;
+
+CREATE TABLE `user_admin` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `_strpass` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `user_name` varchar(100) CHARACTER SET latin1 NOT NULL,
+  `status` float NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+/*Data for the table `user_admin` */
+
+insert  into `user_admin`(`user_id`,`email`,`_strpass`,`user_name`,`status`) values (1,'admin@admin.com','fb5ce7e2dee0fc290f2a6d8567cd9523','Admin Pointers.id',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
